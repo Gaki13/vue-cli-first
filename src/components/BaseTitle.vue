1cd094a415cd52d6f25ed5c23cda9641cb5175a0
Gaki13 <gustavoantoniolopez1311@gmail.com>
2022-05-31 18:07:32 +0200

    Commit con messaggio title

---
+++ src/components/BaseTitle.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>La mia prima volta con Vue CLI</h1>
+</template>
+
+<script>
+export default {
+    name: 'BaseTitle'
+}
+</script>
+
+<style>
+    h1{
+        color: brown;
+    }
+</style>
